7b9de547cac0c3327f3bacecd957c1bdf50ed921
salmanfursi <salmanfursipro@gmail.com>
2025-02-22 05:03:48 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import './global.css'
+import { StatusBar } from 'expo-status-bar'
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <View className="flex-1 items-center justify-center bg-green-300">
+    <Text>Open up App.js to start working on your app!</Text>
+    <Text className='bourder text-red-600'>Open up App.js to start working on your app!</Text>
+    <StatusBar style="auto" />
+  </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+const styles = StyleSheet.create({})
